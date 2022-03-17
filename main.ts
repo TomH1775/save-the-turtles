fdccb9e4b2e09f6790d0b6138e4af3f0de077e55
TomH1775 <98777089+TomH1775@users.noreply.github.com>
2022-03-17 14:47:34 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.showIcon(IconNames.House)
+})
+basic.showString("Save The Turtles")
+basic.forever(function () {
+	
+})
